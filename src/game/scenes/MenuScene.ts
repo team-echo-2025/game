5b5db6f8fb0e378a6d170645ec3b12c12d731239
nboj <60493251+nboj@users.noreply.github.com>
2025-01-28 16:07:20 -0500

    updated main menu

--- src/game/scenes/MenuScene.ts
+++ src/game/scenes/MenuScene.ts
@@ -2,21 +2,43 @@ import { Scene } from 'phaser';
 import { EventBus } from '../EventBus';
 
 export class MenuScene extends Scene {
+    sign: number = 1;
+    background: Phaser.GameObjects.Sprite;
     constructor() {
         super('Game');
     }
 
     preload() {
-        this.load.setPath('assets');
         // load assets
+        this.load.setPath('assets');
+        this.load.image('menu-background', 'menu-background.png');
     }
 
     create() {
-        EventBus.emit('current-scene-ready', this);
+
         // create objects
+        const background = this.add.sprite(0, 0, 'menu-background')
+        background.displayWidth = this.game.canvas.width
+        background.width = this.game.canvas.width
+        background.displayHeight = this.game.canvas.height
+        background.height = this.game.canvas.height
+        background.x = background.x + background.width / 2;
+        background.y = background.y + background.height / 2;
+        this.background = background;
+        EventBus.emit('current-scene-ready', this);
+
+        const title = this.add.text(this.game.canvas.width / 2, this.game.canvas.height / 2, "Escape Game", {
+            "fontSize": "10rem",
+        });
+        title.x = title.x - title.width / 2;
+        title.y = title.y - title.height / 2;
     }
 
     update() {
         // update objects
+        if (this.background.x > this.game.canvas.width / 2) {
+            this.sign *= -1;
+        }
+        this.background.x += 1;
     }
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,10 @@
 import Head from "next/head";
-import { Inter } from "next/font/google";
+import { Inter, Jersey_10 } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import dynamic from "next/dynamic";
 
 const inter = Inter({ subsets: ["latin"] });
+const jersey = Jersey_10({ weight: '400', subsets: ["latin"] });
 
 const AppWithoutSSR = dynamic(() => import("@/App"), { ssr: false });
 
@@ -16,7 +17,7 @@ export default function Home() {
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon.png" />
             </Head>
-            <main className={`${styles.main} ${inter.className}`}>
+            <main className={`${styles.main} ${inter.className} ${jersey.className}`}>
                 <AppWithoutSSR />
             </main>
         </>
