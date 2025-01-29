967d337094be726541ffaa4b799df46371063aed
nboj <christian.auman@outlook.com>
2025-01-29 11:14:22 -0500

    Added directional idles, and better player movement

--- src/game/lib/Player.ts
+++ src/game/lib/Player.ts
@@ -2,6 +2,18 @@ type XY = {
     x: number;
     y: number;
 }
+
+enum Direction {
+    NORTH,
+    NORTHEAST,
+    EAST,
+    SOUTHEAST,
+    SOUTH,
+    SOUTHWEST,
+    WEST,
+    NORTHWEST,
+}
+
 export default class Player {
     #w_key: Phaser.Input.Keyboard.Key | undefined;
     #a_key: Phaser.Input.Keyboard.Key | undefined;
@@ -10,6 +22,9 @@ export default class Player {
     #speed: number = 100;
     #scene: Phaser.Scene;
     #player: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
+    #velocity: XY = { x: 0, y: 0 };
+    #facing: Direction = Direction.SOUTH;
+    #is_moving: boolean = false;
 
     get sprite() {
         return this.#player;
@@ -60,44 +75,79 @@ export default class Player {
         this.#d_key = this.#scene.input.keyboard?.addKey(Phaser.Input.Keyboard.KeyCodes.D)
     }
 
-    #check_key = (key: Phaser.Input.Keyboard.Key | undefined, signs: XY): XY => {
+    #check_key = (key: Phaser.Input.Keyboard.Key | undefined, signs: XY) => {
         if (key && key.isDown) {
-            return signs;
+            this.#velocity.x += signs.x;
+            this.#velocity.y += signs.y;
+        }
+    }
+
+    #get_direction = (): Direction => {
+        if (this.#velocity.x > 0 && this.#velocity.y == 0) {
+            return Direction.EAST;
+        } else if (this.#velocity.x > 0 && this.#velocity.y > 0) {
+            return Direction.SOUTHEAST;
+        } else if (this.#velocity.x < 0 && this.#velocity.y > 0) {
+            return Direction.SOUTHWEST;
+        } else if (this.#velocity.x < 0 && this.#velocity.y < 0) {
+            return Direction.NORTHWEST;
+        } else if (this.#velocity.x < 0 && this.#velocity.y == 0) {
+            return Direction.WEST;
+        } else if (this.#velocity.x > 0 && this.#velocity.y < 0) {
+            return Direction.NORTHEAST;
+        } else if (this.#velocity.y < 0 && this.#velocity.x == 0) {
+            return Direction.NORTH;
+        } else if (this.#velocity.y > 0 && this.#velocity.x == 0) {
+            return Direction.SOUTH;
         } else {
-            return { x: 0, y: 0 }
+            return this.#facing;
         }
     }
 
     #handle_movement = () => {
-        let xy: XY = { x: 0, y: 0 };
-        let temp = this.#check_key(this.#w_key, { x: 0, y: -1 });
-        xy.x += temp.x
-        xy.y += temp.y
-        temp = this.#check_key(this.#a_key, { x: -1, y: 0 });
-        xy.x += temp.x
-        xy.y += temp.y
-        temp = this.#check_key(this.#s_key, { x: 0, y: 1 });
-        xy.x += temp.x
-        xy.y += temp.y
-        temp = this.#check_key(this.#d_key, { x: 1, y: 0 });
-        xy.x += temp.x
-        xy.y += temp.y
-        if (xy.x != 0 || xy.y != 0) {
-            const magnitude = Math.sqrt(Math.pow(Math.abs(xy.x), 2) + Math.pow(Math.abs(xy.y), 2));
-            xy.x /= magnitude;
-            xy.y /= magnitude;
+        this.#velocity.x = 0;
+        this.#velocity.y = 0;
+        this.#check_key(this.#a_key, { x: -1, y: 0 });
+        this.#check_key(this.#w_key, { x: 0, y: -1 });
+        this.#check_key(this.#s_key, { x: 0, y: 1 });
+        this.#check_key(this.#d_key, { x: 1, y: 0 });
+        if (this.#velocity.x != 0 || this.#velocity.y != 0) {
+            const magnitude = Math.sqrt(Math.pow(this.#velocity.x, 2) + Math.pow(this.#velocity.y, 2));
+            this.#velocity.x /= magnitude;
+            this.#velocity.y /= magnitude;
         }
-        this.#player?.setVelocity(xy.x * this.#speed, xy.y * this.#speed);
-        if (xy.y > 0) {
-            this.#player?.play("down", true);
-        } else if (xy.y < 0) {
-            this.#player?.play("up", true);
-        } else if (xy.x > 0) {
-            this.#player?.play("right", true);
-        } else if (xy.x < 0) {
-            this.#player?.play("left", true);
-        } else {
-            this.#player?.setFrame(0);
+        this.#player?.setVelocity(this.#velocity.x * this.#speed, this.#velocity.y * this.#speed);
+        this.#is_moving = this.#velocity.x != 0 || this.#velocity.y != 0;
+        this.#facing = this.#get_direction();
+        switch (this.#facing) {
+            case (Direction.NORTH):
+                if (this.#is_moving) {
+                    this.#player?.play("up", true);
+                } else {
+                    this.#player?.setFrame(8);
+                }
+                break;
+            case (Direction.EAST):
+                if (this.#is_moving) {
+                    this.#player?.play("right", true);
+                } else {
+                    this.#player?.setFrame(16);
+                }
+                break;
+            case (Direction.SOUTH):
+                if (this.#is_moving) {
+                    this.#player?.play("down", true);
+                } else {
+                    this.#player?.setFrame(0);
+                }
+                break;
+            case (Direction.WEST):
+                if (this.#is_moving) {
+                    this.#player?.play("left", true);
+                } else {
+                    this.#player?.setFrame(24);
+                }
+                break;
         }
     }
 
